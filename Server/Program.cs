6b7b40d0e285c4c91fc9ce654e9b985634ad5c47
Leon <leon.kouz@hotmail.com>
2018-03-26 14:58:03 +1100

    Add project files.

---
+++ Server/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Voice
+            //https://www.codeproject.com/Articles/19485/A-Voice-Chat-Application-in-C
+            
+            //SQL
+            //https://docs.microsoft.com/en-us/sql/database-engine/configure-windows/sql-server-2016-express-localdb
+
+            //Server
+            //http://blog.stephencleary.com/2009/04/tcpip-net-sockets-faq.html
+            //https://stackoverflow.com/questions/869744/how-to-write-a-scalable-tcp-ip-based-server - Esac answer
+            //https://www.codeproject.com/Articles/83102/C-SocketAsyncEventArgs-High-Performance-Socket-Cod
+
+        }
+    }
+}
