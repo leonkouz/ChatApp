b6f7667b0e53e841d6e9d0aa9582b31491626521
Leon <leon.kouz@hotmail.com>
2018-04-03 14:20:20 +1000

    Updated BuildMessageFromTcpStringTest_ShouldSplitOnDelimiterCorrectly

--- ClientAppTests/MessageTests.cs
+++ ClientAppTests/MessageTests.cs
@@ -15,16 +15,16 @@ namespace Client.Tests
         public void BuildMessageFromTcpStringTest_ShouldSplitOnDelimiterCorrectly()
         {
             //String to test
-            string tcpString = "this \\0 is \\0 a \\0 test\002-Apr-18 12:54:43 PM\0testUser<EOF>";
-            DateTime tcpTimeStamp = DateTime.Parse("02-Apr-18 12:54:43 PM");
+            string tcpString = "this \\0 is \\0\\0a\\0 test\03/04/2018 2:14:31 PM\0testUser<EOF>";
+            DateTime tcpTimeStamp = DateTime.Parse("03/04/2018 2:14:31 PM");
 
             Message message = Message.BuildMessageFromTcpString(tcpString);
 
             string content = message.Content;
             DateTime timeStamp = message.TimeStamp;
-            string user = "testUser";
+            string user = "testUser<EOF>";
 
-            Assert.AreEqual("this \\0 is \\0 a \\0 test", content);
+            Assert.AreEqual("this \\0 is \\0\\0a\\0 test", content);
             Assert.AreEqual(tcpTimeStamp, timeStamp);
             Assert.AreEqual("testUser<EOF>", user);
         }
