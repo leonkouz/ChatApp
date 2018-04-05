3c4800f1d1cde604d132ba99eef3d0d43e1a874a
Leon <leon.kouz@hotmail.com>
2018-04-05 17:11:39 +1000

    Added WindowViewModel

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -1,20 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using Client.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Client
 {
@@ -30,6 +15,8 @@ namespace Client
         public MainWindow(string userName)
         {
             InitializeComponent();
+
+            this.DataContext = new WindowViewModel(this);
 
             _user = userName;
 
--- Client/ViewModels/WindowViewModel.cs
+++ Client/ViewModels/WindowViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Client.ViewModels
 {
@@ -11,20 +14,165 @@ namespace Client.ViewModels
     /// The View model for the custom window
     /// </summary>
 
-    class WindowViewModel : BaseViewModel
+    public class WindowViewModel : BaseViewModel
     {
+
+        #region Private Fields
 
         /// <summary>
         /// The window this view model controls
         /// </summary>
         private Window _window;
 
+        /// <summary>
+        /// The margin around the window to allow for drop shadow
+        /// </summary>
+        private int _outerMarginSize = 10;
+
+        /// <summary>
+        /// The radius of the edges of the window
+        /// </summary>
+        private int _windowRadius = 10;
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// This size of the resize border around the window
+        /// </summary>
+        public int ResizeBorder { get; set; } = 6;
+
+        /// <summary>
+        /// The size of the resize border around the window, taking into account the outer margin
+        /// </summary>
+        public Thickness ResizeBorderThickness { get { return new Thickness(ResizeBorder + OuterMarginSize); } }
+
+        /// <summary>
+        /// The margin around the window to allow for drop shadow
+        /// </summary>
+        public int OuterMarginSize
+        {
+            get
+            {
+                return _window.WindowState == WindowState.Maximized ? 0 : _outerMarginSize;
+            }
+            set
+            {
+                _outerMarginSize = value;
+            }
+        }
+
+        /// <summary>
+        /// The thickness of the margin around the window to allow for drop shadow
+        /// </summary>
+        public Thickness OuterMarginSizeThickness { get { return new Thickness(OuterMarginSize); } }
+
+        /// <summary>
+        /// The radius of the edges of the window
+        /// </summary>
+        public int WindowRadius
+        {
+            get
+            {
+                return _window.WindowState == WindowState.Maximized ? 0 : _windowRadius;
+            }
+            set
+            {
+                _windowRadius = value;
+            }
+        }
+
+        /// <summary>
+        /// The radius of the edges of the window
+        /// </summary>
+        public CornerRadius WindowCornerRadius { get { return new CornerRadius(WindowRadius); } }
+
+        /// <summary>
+        /// The height of the title bar of the window
+        /// </summary>
+        public int TitleHeight { get; set; } = 42;
+
+        /// <summary>
+        /// The height of the title bar of the window
+        /// </summary>
+        public GridLength TitleHeightGridLength { get { return new GridLength(TitleHeight + ResizeBorder); } }
+
+        #endregion
+
+        #region Commands
+
+        /// <summary>
+        /// The command to minimise the window
+        /// </summary>
+        public ICommand MinimiseCommand { get; set; }
+
+        /// <summary>
+        /// The command to maximise the window
+        /// </summary>
+        public ICommand MaximiseCommand { get; set; }
+
+        /// <summary>
+        /// The command to close the window
+        /// </summary>
+        public ICommand CloseCommand { get; set; }
+
+        /// <summary>
+        /// The command to show the system menu
+        /// </summary>
+        public ICommand MenuCommand { get; set; }
+
+        #endregion
 
         public WindowViewModel(Window window)
         {
+            _window = window;
+
+            //Listen for window state change
+            _window.StateChanged += _window_StateChanged;
+
+            MinimiseCommand = new DelegateCommand(() => _window.WindowState = WindowState.Minimized);
+            MaximiseCommand = new DelegateCommand(() => _window.WindowState ^= WindowState.Maximized);
+            CloseCommand = new DelegateCommand(() => _window.Close());
+            MenuCommand = new DelegateCommand(() => SystemCommands.ShowSystemMenu(_window, GetMousePosition()));
 
         }
 
+        private void _window_StateChanged(object sender, EventArgs e)
+        {
+            //Fire events for all properties that are affected by a resize
+            RaisePropertyChangedEvent(nameof(ResizeBorderThickness));
+            RaisePropertyChangedEvent(nameof(OuterMarginSize));
+            RaisePropertyChangedEvent(nameof(OuterMarginSizeThickness));
+            RaisePropertyChangedEvent(nameof(WindowRadius));
+            RaisePropertyChangedEvent(nameof(WindowCornerRadius));
+        }
+
+        #region Private Helpers
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+
+        /// <summary>
+        /// Gets current mouse position on the screen
+        /// </summary>
+        /// <returns></returns>
+        private static Point GetMousePosition()
+        {
+            Win32Point w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+            return new Point(w32Mouse.X, w32Mouse.Y);
+        }
+
+        #endregion
 
     }
 }
